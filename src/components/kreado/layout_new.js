5d9bf32a95ff63d40ca8f4cfc9a3d20da7b213d5
Yue Zhu <zhuyuejoey@gmail.com>
2024-11-20 17:20:51 +0800

    bugs fixed

--- src/components/kreado/layout_new.js
+++ src/components/kreado/layout_new.js
@@ -3,33 +3,35 @@ import React from 'react';
 
 import TableOfContents from '../common_components/widget-table_of_contents';
 
-import DemoShowCase from '../common_components/hero-section-with-video';
-import CommonTextBlock from '../common_components/text-block';
-import PageDownCTA from '../common_components/call-to-action';
-import TitleLeftThreeModulesRight from '../common_components/how-it-works-with-three-blocks';
-import TitleTopTwoModulesDown from '../common_components/why-choose-us-with-two-huge-blocks';
-import TitleTopSixModulesDown from '../common_components/why-choose-us-with-six-small-blocks';
-import TitleTopThreeFlowDown from '../common_components/how-it-works-with-workflow';
-import TitleLeftFourModulesRight from '../common_components/product-benefits-with-four-blocks';
-import TitleRightTableLeft from '../common_components/call-to-action-with-a-table';
-import FAQ from '../common_components/faqs';
-import ComparisonTable from '../common_components/product_comparison_table';
+import HeroSectionWithVideo from '../common_components/hero-section-with-video';
+import TextBlock from '../common_components/text-block';
+import CallToAction from '../common_components/call-to-action';
+import HowItWorksWithThreeBlocks from '../common_components/how-it-works-with-three-blocks';
+import WhyChooseUsWithTwoHugeBlocks from '../common_components/why-choose-us-with-two-huge-blocks';
+import WhyChooseUsWithSixSmallBlocks from '../common_components/why-choose-us-with-six-small-blocks';
+import HowItWorksWithWorkflow from '../common_components/how-it-works-with-workflow';
+import ProductBenefitsWithFourBlocks from '../common_components/product-benefits-with-four-blocks';
+import CallToActionWithTable from '../common_components/call-to-action-with-a-table';
+import Faqs from '../common_components/faqs';
+import ProductComparisonTable from '../common_components/product_comparison_table';
+import MoreInsightsWithFourCards from '../common_components/more-insights-with-four-cards';
+import ImageBanner from '../common_components/image-banner';
 
 // 更新组件映射表
 const COMPONENT_MAP = {
-  'common_text_block': CommonTextBlock,
-  'cta_bottom': PageDownCTA,
-  'faqs': FAQ,
-  'left_table_right_title': TitleRightTableLeft,
-  'left_title_right_three_modules': TitleLeftThreeModulesRight,
-  'left_title_right_four_modules': TitleLeftFourModulesRight,
-  'top_title_bottom_three_arrow': TitleTopThreeFlowDown,
-  'top_title_bottom_six_modules': TitleTopSixModulesDown,
-  'top_title_bottom_two_modules': TitleTopTwoModulesDown,
-  'demo_showcase': DemoShowCase,
-  'product_comparison_table': ComparisonTable,
-  // 'PageTopTool': PageTopTool,
-  // 'pricing_three_cards': TitleTopPricingDown,
+  TextBlock: TextBlock,
+  CallToAction: CallToAction,
+  Faqs: Faqs,
+  CallToActionWithTable: CallToActionWithTable,
+  HowItWorksWithThreeBlocks: HowItWorksWithThreeBlocks,
+  ProductBenefitsWithFourBlocks: ProductBenefitsWithFourBlocks,
+  HowItWorksWithWorkflow: HowItWorksWithWorkflow,
+  WhyChooseUsWithSixSmallBlocks: WhyChooseUsWithSixSmallBlocks,
+  WhyChooseUsWithTwoHugeBlocks: WhyChooseUsWithTwoHugeBlocks,
+  MoreInsightsWithFourCards: MoreInsightsWithFourCards,
+  ImageBanner: ImageBanner,
+  ProductComparisonTable: ProductComparisonTable,
+  HeroSectionWithVideo: HeroSectionWithVideo
 };
 
 const LayoutKoreadoai = ({ article, keywords }) => {
@@ -63,7 +65,7 @@ const LayoutKoreadoai = ({ article, keywords }) => {
             sections
               .sort((a, b) => a.position - b.position)
               .forEach(section => {
-                if (section.componentName === 'cta_bottom') {
+                if (section.componentName === 'CallToAction') {
                   ctaSection = section;
                 } else {
                   normalSections.push(section);
